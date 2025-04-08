dbfc3960e986c9d8f2c17fe2e1d0f8863686f7fe
이도헌 <vahyeon2516@sonline20.sen.go.kr>
2025-04-08 12:08:20 +0900

    car

--- Assets/2_Scripts/Driver.cs
+++ Assets/2_Scripts/Driver.cs
@@ -13,7 +13,7 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        slowSpeed = moveSpeed * slowSpeedRatio;
+        slowSpeed =  moveSpeed * slowSpeedRatio;
         boostSpeed = moveSpeed * boostSpeedRatio;
     }
 
